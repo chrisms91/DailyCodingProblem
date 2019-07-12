1e39e11494571d2e36df254f70c6025faed2b9e0
Christopher Kim <chrisms91@gmail.com>
2019-07-12 10:21:30 -0700

    Added answer with o(n) time and space complexity for problem04, but was not able to solve with constant space.

---
+++ Problems/01-99/01-19/Problem04/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems._01_99._01_19.Problem04
+{
+	public class Solution
+	{
+		// time o(n), space o(n)
+		public static int FindFirstMissingPositive(int[] nums)
+		{
+			var hs = new HashSet<int>();
+			int result;
+
+			if (nums.Length == 0 || nums == null)
+			{
+				return 1;
+			}
+
+			// build hashset with positive intergers in given array
+			foreach (var num in nums)
+			{
+				if (num > 0)
+				{
+					hs.Add(num);
+				}
+			}
+
+			// iterate all positive integers to find minimum missing integer
+			for (result = 1; result <= Int32.MaxValue; result++)
+			{
+				if (!hs.Contains(result))
+				{
+					break;
+				}
+			}
+			return result;
+		}
+	}
+}
