da2122b8b2da6956a9aec6ec0e2a5ae2d5c92deb
Christopher Kim <chrisms91@gmail.com>
2019-07-15 13:27:23 -0700

    added linear space solution for problem4

--- Problems/01-99/01-19/Problem04/Solution.cs
+++ Problems/01-99/01-19/Problem04/Solution.cs
@@ -36,5 +36,41 @@ namespace Problems._01_99._01_19.Problem04
 			}
 			return result;
 		}
+
+		// time: o(n), space o(1)
+		public static int FindFirstMissingPositiveLinear(int[] nums)
+		{
+			int idx = 0;
+
+			// move all positive numbers to the left, markt the length of active part by idx
+			foreach (var n in nums)
+			{
+				if (n > 0)
+				{
+					nums[idx++] = n;
+				}
+			}
+
+			// the range of result will be [1, idx+1]
+			for (int i = 0; i < idx; i++)
+			{
+				int j = Math.Abs(nums[i]) - 1;
+				// 0 < nums[j] < idx, means j+1 exists, mark visited by making its value negative
+				if(j < idx && nums[j] > 0)
+				{
+					nums[j] *= -1;
+				}
+			}
+
+			for (int i = 0; i < idx; i++)
+			{
+				if (nums[i] > 0)
+				{
+					return i + 1;
+				}
+			}
+
+			return idx + 1;
+		}
 	}
 }
--- Tests/01-99/01-19/Problem04Test.cs
+++ Tests/01-99/01-19/Problem04Test.cs
@@ -14,9 +14,26 @@ namespace Tests._01_99._01_19
 			// Assign
 			int[] nums = { 3, 4, -1, 1 };
 			int[] nums1 = { 7, 8, 9, 10, 11 };
+
 			// Act
 			var result = Problems._01_99._01_19.Problem04.Solution.FindFirstMissingPositive(nums);
 			var result1 = Problems._01_99._01_19.Problem04.Solution.FindFirstMissingPositive(nums1);
+
+			// Assert
+			Assert.AreEqual(2, result);
+			Assert.AreEqual(1, result1);
+		}
+
+		[TestMethod]
+		public void FindFirstMissingPositiveLinear_ShouldReturnCorrectOutput()
+		{
+			// Assign
+			int[] nums = { 3, 4, -1, 1 };
+			int[] nums1 = { 7, 8, 9, 10, 11 };
+
+			// Act
+			var result = Problems._01_99._01_19.Problem04.Solution.FindFirstMissingPositiveLinear(nums);
+			var result1 = Problems._01_99._01_19.Problem04.Solution.FindFirstMissingPositiveLinear(nums1);
 
 			// Assert
 			Assert.AreEqual(2, result);
