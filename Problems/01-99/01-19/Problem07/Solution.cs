d4835c53a374d379019a67432366ef04b70d5eb1
Christopher Kim <chrisms91@gmail.com>
2019-07-16 15:02:58 -0700

    added solution for problem07, but time complexity is exponential.. further optimization is possible using DP in O(n)

---
+++ Problems/01-99/01-19/Problem07/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems._01_99._01_19.Problem07
+{
+	public class Solution
+	{
+		// time: o(2^n), space: o(1)?
+		public static int GetCountDecoding(char[] digits, int length)
+		{
+			// basecase
+			if (length == 0 || length == 1)
+			{
+				return 1;
+			}
+
+			int totalCount = 0;
+
+			// if last char is not 0, add up to total count
+			if (digits[length - 1] > '0')
+			{
+				totalCount = GetCountDecoding(digits, length - 1);
+			}
+
+			// if last two chars are decoded to single number means -> 10 <= n < 27, add up to total recursively..
+			if (digits[length - 2] == '1' || (digits[length - 2] == '2' && digits[length - 1] < '7'))
+			{
+				totalCount += GetCountDecoding(digits, length - 2);
+			}
+
+			return totalCount;
+		}
+
+		// further optimization is possible using DP... (looks similar to fibonacci )
+	}
+}
---
+++ Tests/01-99/01-19/Problem07Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests._01_99._01_19
+{
+	[TestClass]
+	public class Problem07Test
+	{
+		[TestMethod]
+		public void GetCountDecoding_ShouldReturnCorrectCount()
+		{
+			// Assign
+			char[] digits = { '1', '2', '1' };
+			char[] digits1 = { '1', '2', '3', '4' };
+
+			// Act
+			var count = Problems._01_99._01_19.Problem07.Solution.GetCountDecoding(digits, digits.Length);
+			var count1 = Problems._01_99._01_19.Problem07.Solution.GetCountDecoding(digits1, digits1.Length);
+
+			// Assert
+			Assert.AreEqual(3, count);
+			Assert.AreEqual(3, count1);
+		}
+	}
+}
