807088d202605400fa867ea24b2a352fe9ecbba7
Christopher Kim <chrisms91@gmail.com>
2019-07-25 17:59:59 -0700

    added solution for q1_03_URLify

---
+++ Ctci/Ch 01. Arrays and Strings/Q1_03_URLify/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ctci.Ch_01._Arrays_and_Strings.Q1_03_URLify
+{
+	public class Solution
+	{
+
+		private static int CountWhiteSpace(char[] input)
+		{
+			var counter = 0;
+			foreach (var c in input)
+			{
+				if (c == ' ') counter++;
+			}
+			return counter;
+		}
+
+		public static void ReplaceSpaces(char[] input, int length)
+		{
+			var spaceCount = CountWhiteSpace(input);
+			var index = length + spaceCount * 2; // space needed to replace with '%20'
+
+			for (var i = length - 1; i >= 0; i++)
+			{
+				if (i != ' ')
+				{
+					input[index] = input[i];
+					index--;
+				}
+				else
+				{
+					input[index] = '0';
+					input[index - 1] = '2';
+					input[index - 2] = '%';
+					index -= 3;
+				}
+			}
+		}
+	}
+}
---
+++ Tests/Ctci_Test/Ch 01. Arrays and Strings/Q1_03_URLify_Test.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ctci.Ch_01._Arrays_and_Strings.Q1_03_URLify;
+
+namespace Tests.Ctci_Test.Ch_01._Arrays_and_Strings
+{
+	[TestClass]
+	public class Q1_03_URLify_Test
+	{
+		[TestMethod]
+		public void ReplaceSpaces_ShouldReturnCorrectCharArray()
+		{
+			// Assign
+			string inputStr = "abc d e";
+			var charArray = new char[inputStr.Length + 3 * 2 + 1];
+			for (var i = 0; i < inputStr.Length; i++)
+			{
+				charArray[i] = inputStr[i];
+			}
+
+			// Act
+			Solution.ReplaceSpaces(charArray, inputStr.Length);
+
+			// Assert
+			Assert.AreEqual("abc%20d%20e", new string(charArray));
+		}
+	}
+}
