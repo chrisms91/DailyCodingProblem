28a1ccb24a251c34aca6d9010736ada4914e184b
Christopher Kim <chrisms91@gmail.com>
2019-08-05 17:47:14 -0700

    added solution for q1_06_string_compression

---
+++ Ctci/Ch 01. Arrays and Strings/Q1_06_String_Compression/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ctci.Ch_01._Arrays_and_Strings.Q1_06_String_Compression
+{
+	public class Solution
+	{
+		// Time: O(n)
+		// Space: O(n)
+		public static string StringCompression(string str)
+		{
+			str = str.ToLower();
+			var pointerA = str.Length - 1;
+			var pointerB = pointerA;
+			var sb = new StringBuilder();
+			var counter = 1;
+
+			while(pointerA > 0)
+			{
+				if (str[pointerA - 1] == str[pointerB])
+				{
+					counter++;
+					pointerA -= 1;
+				}
+				else
+				{
+					pointerA -= 1;
+					sb.Insert(0, counter);
+					sb.Insert(0, str[pointerB]);
+					pointerB = pointerA;
+					counter = 1;
+				}
+			}
+			sb.Insert(0, counter);
+			sb.Insert(0, str[pointerB]);
+
+			var compressed = sb.ToString();
+			return str.Length < compressed.Length ? str : compressed;
+		}
+	}
+}
---
+++ Tests/Ctci_Test/Ch 01. Arrays and Strings/Q1_06_String_Compressed_Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ctci.Ch_01._Arrays_and_Strings.Q1_06_String_Compression;
+
+namespace Tests.Ctci_Test.Ch_01._Arrays_and_Strings
+{
+	[TestClass]
+	public class Q1_06_String_Compressed_Test
+	{
+		[TestMethod]
+		public void StringCompression_ShouldReturnCompressedString()
+		{
+			// Assign
+			var testStr = "aabcccccaaa";
+			var testStr1 = "abbccccccccdee";
+
+			// Act
+			var res = Solution.StringCompression(testStr);
+			var res1 = Solution.StringCompression(testStr1);
+
+			// Assert
+			Assert.AreEqual("a2b1c5a3", res);
+			Assert.AreEqual("a1b2c8d1e2", res1);
+		}
+	}
+}
