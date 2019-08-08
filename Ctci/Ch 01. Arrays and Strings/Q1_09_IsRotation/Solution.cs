64d9fcfc004702ecd2b18011c849b4b96829af8c
Christopher Kim <chrisms91@gmail.com>
2019-08-07 23:09:33 -0700

    q1_09 solution added

---
+++ Ctci/Ch 01. Arrays and Strings/Q1_09_IsRotation/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Ctci.Ch01.ArraysandStrings.Q1_09_IsRotation
+{
+    public static class Solution
+    {
+        public static bool IsRotation(string s1, string s2)
+        {
+            var sb = new StringBuilder();
+            sb.Append(s2).Append(s2);
+
+            var concatStr = sb.ToString();
+
+            return ctci.Library.AssortedMethods.IsSubString(concatStr, s1);
+        }
+    }
+}
---
+++ Tests/Ctci_Test/Ch 01. Arrays and Strings/Q1_09_IsRotation_Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ctci.Ch01.ArraysandStrings.Q1_09_IsRotation;
+
+namespace Tests.Ctci_Test.Ch01.ArraysandStrings
+{
+    [TestClass]
+    public class Q1_09_IsRotation_Test
+    {
+        [TestMethod]
+        public void IsRotation_ShouldReturnTrue()
+        {
+            // Assign
+            var testSet = new string[] { "waterbottle", "erbottlewat" };
+            var testSet1 = new string[] { "lifestyle", "tylelifes" };
+
+            // Act
+            var res = Solution.IsRotation(testSet[0], testSet[1]);
+            var res1 = Solution.IsRotation(testSet1[0], testSet1[1]);
+
+            // Assert
+            Assert.AreEqual(true, res);
+            Assert.AreEqual(true, res1);
+        }
+
+        [TestMethod]
+        public void IsRotation_ShouldReturnFalse()
+        {
+            // Assign
+            var testSet = new string[] { "waterbottle", "erbottlewta" };
+
+            // Act
+            var res = Solution.IsRotation(testSet[0], testSet[1]);
+
+            // Assert
+            Assert.AreEqual(false, res);
+        }
+    }
+}
