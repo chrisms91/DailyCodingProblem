08059168d5cfded4c26cfbd5b23a62e86bf15645
Christopher Kim <chrisms91@gmail.com>
2019-08-19 23:15:22 -0700

    added initial solution for kthToLast

---
+++ Ctci/Ch 02. Linked Lists/Q2_02_KthToLast/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ctci.ctci.Library;
+
+namespace Ctci.Ch02.LinkedLists.Q2_02_KthToLast
+{
+    public class Solution
+    {
+        public static LinkedListNode ReturnKthToLast(LinkedListNode node, int k)
+        {
+            if (node == null) return node;
+
+            // Calculate Length of Linked List
+            var ptr = node;
+            var length = 1;
+
+            while (ptr != null)
+            {
+                ptr = ptr.Next;
+                length++;
+            }
+
+            // move ptr forward to Length - k
+            var position = length - k;
+            var targetNode = node;
+            while (position >= 0)
+            {
+                targetNode = targetNode.Next;
+                position--;
+            }
+
+            // return ptr
+            return targetNode;
+        }
+    }
+}
