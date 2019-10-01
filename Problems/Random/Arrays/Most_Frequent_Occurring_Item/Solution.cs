24a24d146779f23ab83491c5ae933b9ca6184332
Christopher Kim <chrisms91@gmail.com>
2019-09-30 17:38:03 -0700

    daily coding problem

---
+++ Problems/Random/Arrays/Most_Frequent_Occurring_Item/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Random.Arrays.Most_Frequent_Occurring_Item
+{
+	public class Solution
+	{
+		public static int? MostFrequent(int[] givenArray)
+		{
+			if (givenArray.Length == 0) return null;
+
+			var hm = new Dictionary<int, int>();
+			int? maxItem = null;
+			var maxCount = -1;
+
+
+			foreach (var key in givenArray)
+			{
+				if (hm.ContainsKey(key))
+				{
+					var currentValue = hm[key];
+					currentValue++;
+					hm[key] = currentValue;
+				}
+				else
+				{
+					hm.Add(key, 1);
+				}
+
+				if (hm[key] > maxCount)
+				{
+					maxCount = hm[key];
+					maxItem = key;
+				}
+			}
+
+			return maxItem;
+		}
+	}
+}
---
+++ Tests/Random_Test/Arrays_Test/Most_Frequent_Occurring_Item_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Problems.Random.Arrays.Most_Frequent_Occurring_Item;
+
+namespace Tests.Random_Test.Arrays_Test
+{
+	[TestClass]
+	public class Most_Frequent_Occurring_Item_Test
+	{
+		[TestMethod]
+		public void Most_Frequent_ShouldReturnItem()
+		{
+			// Assign
+			// mostFrequent(array1) should return 1.
+			int[] array1 = { 1, 3, 1, 3, 2, 1 };
+			// mostFrequent(array2) should return 3.
+			int[] array2 = { 3, 3, 1, 3, 2, 1 };
+			// mostFrequent(array3) should return -1.
+			int[] array3 = { };
+			// mostFrequent(array4) should return 0.
+			int[] array4 = { 0 };
+			// mostFrequent(array5) should return -1.
+			int[] array5 = { 0, -1, 10, 10, -1, 10, -1, -1, -1, 1 };
+
+			// Act
+			var res1 = Solution.MostFrequent(array1);
+			var res2 = Solution.MostFrequent(array2);
+			var res3 = Solution.MostFrequent(array3);
+			var res4 = Solution.MostFrequent(array4);
+			var res5 = Solution.MostFrequent(array5);
+
+			// Assert
+			Assert.AreEqual(1, res1);
+			Assert.AreEqual(3, res2);
+			Assert.AreEqual(null, res3);
+			Assert.AreEqual(0, res4);
+			Assert.AreEqual(-1, res5);
+		}
+	}
+}
