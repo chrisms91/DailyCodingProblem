08832d7d9e72da2966138ec34ff15e1db275f97f
Christopher Kim <chrisms91@gmail.com>
2019-10-07 16:36:48 -0700

    non repeating character

---
+++ Problems/Random/Strings/Non_Repeating_Character/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Random.Strings.Non_Repeating_Character
+{
+	public class Solution
+	{
+		public static char? NonRepeatingCharacter(string str)
+		{
+			if (str.Length == 0) return null;
+
+			var dict = new Dictionary<char, int>();
+
+			foreach (var chr in str)
+			{
+				if (dict.ContainsKey(chr))
+				{
+					var temp = dict[chr]++;
+					dict[chr] = temp;
+				}
+				else
+				{
+					dict.Add(chr, 1);
+				}
+			}
+
+			foreach (var chr in str)
+			{
+				if (dict[chr] == 1)
+					return chr;
+			}
+
+			return null;
+		}
+	}
+}
