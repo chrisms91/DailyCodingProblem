db2ac4396b6606daf7b610a0859110d63a6b2335
Christopher Kim <chrisms91@gmail.com>
2019-10-09 17:40:28 -0700

    one away strings solution and testcases

---
+++ Problems/Random/Strings/One_Away_Strings/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Random.Strings.One_Away_Strings
+{
+	public class Solution
+	{
+		public static bool IsOneAway(string s1, string s2)
+		{
+			if ((s1 == s2) || (s1.Length == 1 && s2.Length == 1)) return true;
+
+			if (s1.Length == s2.Length)
+			{
+				return IsOneAwaySameLength(s1, s2);
+			}
+			else if (s1.Length > s2.Length)
+			{
+				return IsOneAwayDiffLength(s1, s2);
+			}
+			else
+			{
+				return IsOneAwayDiffLength(s2, s1);
+			}
+		}
+
+		private static bool IsOneAwaySameLength(string s1, string s2)
+		{
+			var differentAlready = false;
+			for (var i = 0; i < s1.Length; i++)
+			{
+				if (s1[i] != s2[i])
+				{
+					if (differentAlready)
+					{
+						return false;
+					}
+					differentAlready = true;
+				}
+			}
+			return true;
+		}
+
+		private static bool IsOneAwayDiffLength(string longer, string shorter)
+		{
+			if (longer.Length - shorter.Length > 1) return false;
+
+			var ptl = 0;
+			var pts = 0;
+			var differentAlready = false;
+
+			while (pts < shorter.Length)
+			{
+				if (longer[ptl] != shorter[pts])
+				{
+					if (differentAlready)
+					{
+						return false;
+					}
+					differentAlready = true;
+					ptl++;
+				}
+				else
+				{
+					ptl++;
+					pts++;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ Tests/Random_Test/Strings_Test/One_Away_Strings_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Random.Strings.One_Away_Strings;
+
+namespace Tests.Random_Test.Strings_Test
+{
+	[TestClass]
+	public class One_Away_Strings_Test
+	{
+		[TestMethod]
+		public void IsOneAway_ShouldReturnTrue()
+		{
+			// Assign
+			string[] case1 = { "abcde", "abcd" };
+			string[] case2 = { "abcd", "abcde" };
+			string[] case3 = { "a", "a" };
+			string[] case4 = { "abcdef", "abqdef" };
+			string[] case5 = { "abcdef", "abcde" };
+
+			// Act
+			var result1 = Solution.IsOneAway(case1[0], case1[1]);
+			var result2 = Solution.IsOneAway(case2[0], case2[1]);
+			var result3 = Solution.IsOneAway(case3[0], case3[1]);
+			var result4 = Solution.IsOneAway(case4[0], case4[1]);
+			var result5 = Solution.IsOneAway(case5[0], case5[1]);
+
+			// Assert
+			Assert.AreEqual(result1, true);
+			Assert.AreEqual(result2, true);
+			Assert.AreEqual(result3, true);
+			Assert.AreEqual(result4, true);
+			Assert.AreEqual(result5, true);
+		}
+
+		[TestMethod]
+		public void IsOneAway_ShouldReturnFalse()
+		{
+			// Assign
+			string[] case1 = { "aaa", "abc" };
+			string[] case2 = { "abcde", "abc" };
+			string[] case3 = { "abc", "abcde" };
+			string[] case4 = { "abc", "bcc" };
+
+			// Act
+			var result1 = Solution.IsOneAway(case1[0], case1[1]);
+			var result2 = Solution.IsOneAway(case2[0], case2[1]);
+			var result3 = Solution.IsOneAway(case3[0], case3[1]);
+			var result4 = Solution.IsOneAway(case4[0], case4[1]);
+
+			// Assert
+			Assert.AreEqual(result1, false);
+			Assert.AreEqual(result2, false);
+			Assert.AreEqual(result3, false);
+			Assert.AreEqual(result4, false);
+		}
+	}
+}
