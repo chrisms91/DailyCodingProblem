21e62547bc54195536df966e206019bc7dad26b2
Christopher Kim <chrisms91@gmail.com>
2019-10-31 17:43:25 -0700

    long time no see..assign numbers to minesweeper solution

---
+++ Problems/Random/Two_Dimensional_Array/Assign_Numbers_In_MineSweeper/README.md
@@ -0,0 +1,15 @@
+﻿### Assign Numbers in Minesweeper
+
+Implement a function that assigns correct numbers in a field of Minesweeper, which is represented as a 2 dimensional array.
+
+### Example
+
+The size of the field is 3x4, and there are bombs at the positions [0, 0] (row index = 0, column index = 0) and [0, 1] (row index = 0, column index = 1)
+
+Then, the resulting field should be:
+
+```
+[[-1, -1, 1, 0], 
+[2, 2, 1, 0], 
+[0, 0, 0, 0]]
+```
---
+++ Problems/Random/Two_Dimensional_Array/Assign_Numbers_In_MineSweeper/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Random.Two_Dimensional_Array.Assign_Numbers_In_MineSweeper
+{
+	public class Solution
+	{
+		public static int[,] MineSweeper(int[][] bombs, int numRows, int numCols)
+		{
+			// initialize 2d array for field
+			int[,] field = new int[numRows,numCols];
+
+			// iterate each bomb indices
+			foreach (int[] bomb in bombs)
+			{
+				var rowIndex = bomb[0];
+				var colIndex = bomb[1];
+
+				// set bomb location to -1
+				field[rowIndex, colIndex] = -1;
+
+				for (int i = rowIndex - 1; i <= rowIndex + 1; i++)
+				{
+					for (int j = colIndex - 1; j <= colIndex + 1; j++)
+					{
+						if (i >= 0 && i < numRows && j >= 0 && j < numCols && field[i, j] != -1)
+						{
+							field[i, j] += 1;
+						}
+					}
+				}
+			}
+
+			return field;
+		}
+
+		public static void Print2DArray<T>(T[,] matrix)
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Console.Write(matrix[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
