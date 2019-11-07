95d05811b39af85e369b8026ba4f65627bc91d34
Christopher Kim <chrisms91@gmail.com>
2019-11-06 22:25:38 -0800

    added solution for nth_from_end

---
+++ Problems/Random/Linked_Lists_And_Trees/Nth_From_End/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ctci.ctci.Library;
+
+namespace Problems.Random.Linked_Lists_And_Trees.Nth_From_End
+{
+    public class Solution
+    {
+        public static LinkedListNode NthFromEnd(LinkedListNode head, int n)
+        {
+            var ptr1 = head;
+            var ptr2 = head;
+
+            // move one pointer1 to nth node
+            for (var i = 0; i < n; i++)
+            {
+                if (ptr1 == null) return null;
+                ptr1 = ptr1.Next;
+            }
+
+            // move both pointer until pointer1 reaches the end of the list
+            while (ptr1 != null)
+            {
+                ptr1 = ptr1.Next;
+                ptr2 = ptr2.Next;
+            }
+
+            // return pointer2
+            return ptr2;
+        }
+    }
+}
