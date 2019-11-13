a8d86ff71ff03f7c354d86f897169c638d05bd0a
Christopher Kim <chrisms91@gmail.com>
2019-11-12 17:57:30 -0800

    nth from end test cases

---
+++ Tests/Random_Test/Linked_Lists_And_Trees_Test/Nth_From_End_Test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ctci.ctci.Library;
+using Problems.Random.Linked_Lists_And_Trees.Nth_From_End;
+
+namespace Tests.Random_Test.Linked_Lists_And_Trees_Test
+{
+	[TestClass]
+	public class Nth_From_End_Test
+	{
+		[TestMethod]
+		public void NthFromEnd_ShouldReturnCorrectNode()
+		{
+			// Assign
+			var head = LinkedListNode.CreateLinkedList(new int[] { 7, 6, 5, 4, 3, 2, 1 });
+			var head2 = LinkedListNode.CreateLinkedList(new int[] { 1, 2, 3, 4 });
+
+			// Act
+			var res = Solution.NthFromEnd(head, 1); // should return 1
+			var res1 = Solution.NthFromEnd(head, 5); // should return 5
+			var res2 = Solution.NthFromEnd(head2, 2); // should return 3
+			var res3 = Solution.NthFromEnd(head2, 5); // should return null
+
+			// Assert
+			Assert.AreEqual(res.Data, 1);
+			Assert.AreEqual(res1.Data, 5);
+			Assert.AreEqual(res2.Data, 3);
+			Assert.IsNull(res3);
+
+
+
+		}
+	}
+}
