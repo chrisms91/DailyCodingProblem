a9451b9d716d2047e7c62f2e7a9b83e362d43930
Christopher Kim <chrisms91@gmail.com>
2019-11-13 17:20:44 -0800

    added test cases for isBST

--- Problems/Random/Linked_Lists_And_Trees/Is_This_Binary_Search_Tree/Solution.cs
+++ Problems/Random/Linked_Lists_And_Trees/Is_This_Binary_Search_Tree/Solution.cs
@@ -2,34 +2,34 @@
 using Problems.Common;
 namespace Problems.Random.Linked_Lists_And_Trees.Is_This_Binary_Search_Tree
 {
-    public class Solution
-    {
-        public static bool IsBST(Node<int> head)
-        {
-            return IsBST(head, null, null);
-        }
+	public class Solution
+	{
+		public static bool IsBST(Node<int> head)
+		{
+			return IsBST(head, null, null);
+		}
 
-        public static bool IsBST(Node<int> node, int? leftHead, int? rightHead)
-        {
-            // base cases
-            if (leftHead != null && node.Value > leftHead) return false;
-            if (rightHead != null && node.Value < rightHead) return false;
+		public static bool IsBST(Node<int> node, int? leftHead, int? rightHead)
+		{
+			// base cases
+			if (leftHead != null && node.Value > leftHead) return false;
+			if (rightHead != null && node.Value < rightHead) return false;
 
-            bool isLeftSubBST = true;
-            bool isRightSubBST = true;
+			bool isLeftSubBST = true;
+			bool isRightSubBST = true;
 
-            // check left subtree if every nodes are lower than leftHead
-            // and right subtree if every nodes are larger than rightHead recursively
-            if (node.Left != null)
-            {
-                isLeftSubBST = IsBST(node.Left, node.Value, rightHead);
-            }
-            if (node.Right != null)
-            {
-                isRightSubBST = IsBST(node.Right, leftHead, node.Value);
-            }
+			// check left subtree if every nodes are lower than leftHead
+			// and right subtree if every nodes are larger than rightHead recursively
+			if (node.Left != null)
+			{
+				isLeftSubBST = IsBST(node.Left, node.Value, rightHead);
+			}
+			if (node.Right != null)
+			{
+				isRightSubBST = IsBST(node.Right, leftHead, node.Value);
+			}
 
-            return isLeftSubBST && isRightSubBST;
-        }
-    }
+			return isLeftSubBST && isRightSubBST;
+		}
+	}
 }
---
+++ Tests/Random_Test/Linked_Lists_And_Trees_Test/Is_This_Binary_Search_Tree_Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Common;
+using Problems.Random.Linked_Lists_And_Trees.Is_This_Binary_Search_Tree;
+namespace Tests.Random_Test.Linked_Lists_And_Trees_Test
+{
+	[TestClass]
+	public class Is_This_Binary_Search_Tree_Test
+	{
+		[TestMethod]
+		public void IsBST_ShouldReturnTrue()
+		{
+			// Assign
+			//			3
+			//		/   \
+			//	 1     5
+			//	/ \   / \
+			// 0   2 4   6
+			var root = new Node<int>(3, 
+				new Node<int>(1, new Node<int>(0), new Node<int>(2)), 
+				new Node<int>(5, new Node<int>(4), new Node<int>(6)));
+
+			// Act
+			var result = Solution.IsBST(root);
+
+			// Assert
+			Assert.AreEqual(result, true);
+		}
+
+		[TestMethod]
+		public void IsBST_ShouldReturnFalse()
+		{
+			// Assign
+			//     0
+			//    / \
+			//   1   2
+			//  / \ / \
+			// 3  4 5  6
+			var root = new Node<int>(0, 
+				new Node<int>(1, new Node<int>(3), new Node<int>(4)), 
+				new Node<int>(2, new Node<int>(5), new Node<int>(6)));
+
+			// Act
+			var result = Solution.IsBST(root);
+
+			// Assert
+			Assert.AreEqual(result, false);
+		}
+	}
+}
