dcb7609e4d9f61ae538dec48a4f509020de59151
Christopher Kim <chrisms91@gmail.com>
2019-11-19 16:29:47 -0800

    added solution for lowest common ancestor

---
+++ Problems/Random/Linked_Lists_And_Trees/Lowest_Common_Ancestor/README.md
@@ -0,0 +1,22 @@
+﻿### Lowest Common Ancestor
+
+Find the lowest common ancestor of two items in a binary tree
+
+#### Assumptions:
+- Each value in the tree is unique (there are no two nodes with the same value)
+- Each node has at most two children, left and right
+- You do not have a level attribute in each of your node
+- Each node has pointers to left and right children, but there is no back link
+
+#### Examples:
+```
+head =  5
+       / \
+      1   4
+     / \ / \
+    3  8 9  2
+	 / \
+	6   7
+```
+- LCA of 8 and 7 is 1
+- LCA of 4 and 2 is 4
---
+++ Problems/Random/Linked_Lists_And_Trees/Lowest_Common_Ancestor/Solution.cs
@@ -0,0 +1,59 @@
+﻿using Problems.Common;
+using System.Collections.Generic;
+
+namespace Problems.Random.Linked_Lists_And_Trees.Lowest_Common_Ancestor
+{
+	public class Solution
+	{
+		public static Node<int> Lca(Node<int> root, int x, int y)
+		{
+			var pathToX = PathToNode(root, x);
+			var pathToY = PathToNode(root, y);
+			if (pathToX == null || pathToY == null) return null;
+
+			// compare two paths to find lca
+			Node<int> currentCommonAncestor = null;
+			while (pathToX.Count != 0 && pathToY.Count != 0)
+			{
+				var xPop = pathToX.Pop();
+				var yPop = pathToY.Pop();
+				if (xPop == yPop)
+				{
+					currentCommonAncestor = xPop;
+				}
+				else
+				{
+					break;
+				}
+			}
+			return currentCommonAncestor;
+		}
+
+		private static Stack<Node<int>> PathToNode(Node<int> root, int x)
+		{
+			if (root == null) return null;
+			if (root.Value == x)
+			{
+				Stack<Node<int>> stack = new Stack<Node<int>>();
+				stack.Push(root);
+				return stack;
+			}
+
+			Stack<Node<int>> leftPathStack = PathToNode(root.Left, x);
+			if (leftPathStack != null)
+			{
+				leftPathStack.Push(root);
+				return leftPathStack;
+			}
+
+			Stack<Node<int>> rightPathStack = PathToNode(root.Right, x);
+			if (rightPathStack != null)
+			{
+				rightPathStack.Push(root);
+				return rightPathStack;
+			}
+
+			return null;
+		}
+	}
+}
