5cb5c11e421b6a9372dea841731487a4ec85ec53
Christopher Kim <chrisms91@gmail.com>
2020-02-10 22:43:58 -0800

    add unit testing case

---
+++ Tests/InterviewBitTest/ArraysTest/AddOneToNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.InterviewBit.Arrays.AddOneToNumber;
+
+namespace Tests.InterviewBitTest.ArraysTest
+{
+    [TestClass]
+    public class AddOneToNumberTest
+    {
+        [TestMethod]
+        public void AddOneToNumber_ShouldReturnRightArray()
+        {
+            // assign
+            var testList = new List<int> { 1, 2, 3 };
+            var testList1 = new List<int> { 9, 9, 9 };
+            var expected = new List<int> { 1, 2, 4 };
+            var expected1 = new List<int> { 1, 0, 0, 0 };
+
+            // act
+            var result = Solution.PlusOne(testList);
+            var result1 = Solution.PlusOne(testList1);
+
+            // assert
+            CollectionAssert.AreEqual(result, expected);
+            CollectionAssert.AreEqual(result1, expected1);
+        }
+    }
+}
